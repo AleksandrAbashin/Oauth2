c0edacd2777413c8ead2de4272d4bdd32172cbfc
AleksandrAbashin <abashin.aleksandr@mail.ru>
2019-03-29 15:36:54 +0300

    commit

--- src/main/java/com/firstline/oauth/repo/UserDetailsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.firstline.oauth.repo;
-
-import com.firstline.oauth.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDetailsRepo extends JpaRepository<User, String> {
-}
